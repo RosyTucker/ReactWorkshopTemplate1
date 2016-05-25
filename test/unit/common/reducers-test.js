66a2dd7845dc9be3ea68e7a3c05398a8cf405bb1
Rose Tucker <rosy533@gmail.com>
2016-05-25 20:54:46 +0100

    Add tests for reducer

---
+++ test/unit/common/reducers-test.js
@@ -0,0 +1,34 @@
+import { React, expect, Enzyme } from '../../TestHelpers';
+import { ActionTypes } from '../../../src/js/common/Constants';
+import reducers from '../../../src/js/common/reducers';
+
+describe('reducers.js', () => {
+    it('should set the user in the state to an empty object if action type is not receive user', () => {
+        const action = { type: 'NOT_RECEIVE_USER' };
+
+        const updatedState = reducers(undefined, action);
+
+        expect(updatedState).to.deep.equal({ user: {} });
+    });
+
+    it('should set the user in the state when action type is receive user', () => {
+        const user = { someUser: 'some value' };
+        const action = { type: ActionTypes.RECEIVE_USER, user };
+
+        const updatedState = reducers(undefined, action);
+
+        expect(updatedState).to.deep.equal({ user });
+    });
+
+    it('should append received user to existing user', () => {
+        const user = { someUser: 'some value' };
+        const action = { type: ActionTypes.RECEIVE_USER, user };
+        const existingState = { user: { someOtherKey: 'some other value' } };
+
+        const updatedState = reducers(existingState, action);
+
+        expect(updatedState).to.deep.equal({
+            user: { someUser: 'some value', someOtherKey: 'some other value' }
+        });
+    });
+});
