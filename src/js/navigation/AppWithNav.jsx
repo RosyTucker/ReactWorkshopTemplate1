c94492a659266cb842774443a33c11b6a628abaa
Rose Tucker <rosy533@gmail.com>
2016-05-29 13:54:45 +0100

    Pass isClear property to nav

--- src/js/navigation/AppWithNav.jsx
+++ src/js/navigation/AppWithNav.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import AppRoutes  from './AppRoutes';
+import { connect } from 'react-redux';
 import Nav from './Nav';
 
-const AppWithNav = props => (
-    <div className="app-with-nav">
-        <Nav />
-        {props.children}
-    </div>
-);
+const AppWithNav = props => {
+    const isClear = props.location.pathname === AppRoutes.home;
+    return (
+        <div className="app-with-nav">
+            <Nav isClear={isClear} />
+            {props.children}
+        </div>
+    );
+};
 
 AppWithNav.propTypes = {
-    children: React.PropTypes.any
+    children: React.PropTypes.any,
+    location: React.PropTypes.object.isRequired
 };
 
 export default AppWithNav;
-
