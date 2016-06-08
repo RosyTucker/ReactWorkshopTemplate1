4b9e6a5ad4e43a3c87d468f3872d571fa62e35ea
Rose Tucker <rosy533@gmail.com>
2016-06-08 20:01:12 +0100

    Add initial search items from workshop 1 …
    … as a template

--- src/js/home/Home.jsx
+++ src/js/home/Home.jsx
@@ -3,12 +3,45 @@ import React from 'react';
 import '../../sass/home.scss';
 
 const data = {
-    title: 'some title'
+    title: 'Search',
+    searchButtonText: 'Search',
+    searchFieldPlaceholder: 'some search',
+    moreButtonText: 'More',
+    results: [
+        { id: 1, link: '/somelink1', content: 'Some result 1' },
+        { id: 2, link: '/somelink2', content: 'Some result 2' },
+        { id: 3, link: '/somelin3', content: 'Some result 3' }
+    ]
+};
+
+
+const SearchResult = ({ link, resultContent }) => (
+    <li><a href={link} className="search-result">{resultContent}</a></li>
+);
+
+const SearchResults = ({ results }) => {
+    const resultItems = results.map(result => (
+        <SearchResult key={result.id} link={result.link} resultContent={result.content} />
+    ));
+
+    return (
+        <ul className="search-results">
+            {resultItems}
+        </ul>
+    );
 };
 
 const Home = () => (
-    <div className="some-class">
-        {data.title}
+    <div className="search">
+        <h1>{data.title}</h1>
+        <div className="search-bar">
+            <form>
+                <input type="text" placeholder={data.searchFieldPlaceholder}/>
+                <button>{data.searchButtonText}</button>
+            </form>
+        </div>
+        <SearchResults results={data.results} />
+        <a href="" className="search-load-more">{data.moreButtonText}</a>
     </div>
 );
 
