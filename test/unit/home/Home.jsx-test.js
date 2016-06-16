448cbc940369acbeb66ba6d0d2362f7d297797a4
Alberto Forni <alberto.forn@gmail.com>
2016-06-16 13:45:51 +0100

    Add jsdom and watch flag for tests

--- test/unit/home/Home.jsx-test.js
+++ test/unit/home/Home.jsx-test.js
@@ -1,2 +1,44 @@
-describe('Home.jsx', () => {
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
+import React, { Component, PropTypes } from 'react';
+import Home from '../../../src/js/home/Home.jsx';
+
+class Element extends Component {
+    componentDidMount() {
+        this.props.onMount('string');
+    }
+
+    render() {
+        const { onClick } = this.props;
+
+        return (
+            <button onClick={onClick}>Click me</button>
+        );
+    }
+}
+
+Element.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    onMount: PropTypes.func.isRequired
+};
+
+describe('<Element />', () => {
+    it('calls onClick when the button gets clicked', () => {
+        const onClick = sinon.spy();
+
+        const wrapper = shallow(<Element onClick={onClick} onMount={() => {}} />);
+        wrapper.find('button').simulate('click');
+
+        expect(onClick.calledOnce).to.equal(true);
+    });
+
+    it('calls onMount when the button gets clicked', () => {
+        const onMount = sinon.spy();
+
+        const wrapper = mount(<Element onClick={() => {}} onMount={onMount} />);
+        wrapper.find('button').simulate('click');
+
+        expect(onMount.calledOnce).to.equal(true);
+    });
 });
